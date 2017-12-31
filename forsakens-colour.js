2eba0ee2efc69f1d57e8b6198e836904a157885d
Forsakens <rama.bully@yahoo.co.id>
2017-12-31 12:18:22 +0700

    Add files via upload

---
+++ forsakens-colour.js
@@ -0,0 +1,24 @@
+/*!
+ * Hacking Forsakens
+ * http://webforsakens.com
+ *
+ * Copyright 2010, Rama Forsakens
+ * Pemangkat Community
+ * http://webforsakens.org/739874832432/license
+ *
+ * Colour-Random.js
+ * LOL
+ * Copyright 2010, Hacking Forsakens Foundation
+ * Released under the MIT, BSD, and GPL Licenses.
+ *
+ * Date: Sat Feb 13 22:33:48 2010 -0500
+ */
+
+
+
+
+javascript:i=0;c=["red","green","blue","yellow","magenta","orange"," pink","violet"]; a=document.links;setInterval('i++;a[i % document.links.length].style.color=c[i % c.length]',1);void(0);         
+
+
+
+
